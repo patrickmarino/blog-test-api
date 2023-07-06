02979db79975f28c2edb8a5732bc2e14543da0f1
Patrick Marino <pmmarino@multisyscorp.com>
2023-07-06 23:12:43 +0800

    add factory and fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\CategoryFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+       CategoryFactory::createMany(5);
+    }
+}
